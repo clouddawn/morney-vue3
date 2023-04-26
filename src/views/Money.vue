ea30df26e70bbf2f66010fcbc55571ef212cdde5
clouddawn <1993456181@qq.com>
2023-04-26 12:35:16 +0800

    橙子å记账

---
+++ src/views/Money.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    money
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Money",
+  components: {},
+  data() {
+    return {};
+  },
+  mounted() {
+
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
