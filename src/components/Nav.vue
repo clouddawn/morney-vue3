bb9f6f0e1d6c09432ed55a76fb68cc41801df37a
clouddawn <1993456181@qq.com>
2023-04-27 16:55:37 +0800

    navBar完成

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,74 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>
-    <router-link to="/labels">标签</router-link>
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/money"
+                 class="icon"
+                 active-class="selected">
+      <img class="orange-img" src="@/assets/money-orange.svg" alt="">
+      <img class="black-img" src="@/assets/money.svg" alt="">
+      <div class="icon-font">记账</div>
+    </router-link>
+
+    <router-link to="/labels"
+                 class="icon"
+                 active-class="selected">
+      <img class="orange-img" src="@/assets/label-orange.svg" alt="">
+      <img class="black-img" src="@/assets/label.svg" alt="">
+      <div class="icon-font">标签</div>
+    </router-link>
+
+    <router-link to="/statistics"
+                 class="icon"
+                 active-class="selected">
+      <img class="orange-img" src="@/assets/statistics-orange.svg" alt="">
+      <img class="black-img" src="@/assets/statistics.svg" alt="">
+      <div class="icon-font">统计</div>
+    </router-link>
   </div>
 </template>
 
-<script>
+<script setup>
 
 </script>
 
 <style lang="scss" scoped>
 .nav {
-  height: 40px;
+  display: flex;
+  justify-content: space-around;
+  box-shadow: 0 0 3px rgba(0, 0, 0, .25);
+  padding: 2px 0;
+
+  .icon {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    img {
+      width: 32px;
+      height: 32px;
+      margin-bottom: 2px;
+    }
+
+    .orange-img {
+      display: none;
+    }
+
+    .icon-font {
+      font-size: 12px;
+    }
+  }
+
+  .selected {
+    .orange-img {
+      display: block;
+    }
+
+    .black-img {
+      display: none;
+    }
+
+    .icon-font {
+      color: #ff6600;
+    }
+  }
 }
 </style>
